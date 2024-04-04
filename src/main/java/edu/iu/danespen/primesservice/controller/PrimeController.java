7a8c7b28efa848b368696bb3187f887b4661aa2d
Spencer, Dane Sullivan <danespen@iu.edu>
2024-04-04 16:17:24 -0400

    added rabbit

--- src/main/java/edu/iu/danespen/primesservice/controller/PrimeController.java
+++ src/main/java/edu/iu/danespen/primesservice/controller/PrimeController.java
@@ -1,5 +1,6 @@
 package edu.iu.danespen.primesservice.controller;
 
+import edu.iu.danespen.primesservice.rabbitmq.MQSender;
 import edu.iu.danespen.primesservice.service.IPrimesService;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,12 +9,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/primes")
 public class PrimeController {
     IPrimesService primesService;
-    public PrimeController(IPrimesService primesService){
+    private final MQSender mqSender;
+    public PrimeController(IPrimesService primesService, MQSender mqSender){
         this.primesService = primesService;
+        this.mqSender = mqSender;
     }
 
     @GetMapping("/{n}")
     public boolean isPrime(@PathVariable int n){
-        return primesService.isPrime(n);
-    }
-}
+        boolean result = primesService.isPrime(n);
+        mqSender.sendMessage(n, result);
+        return result;
+    }}
