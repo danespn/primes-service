1b4a3aa544db5e4cef903d736f238dcb1ee9b3f4
Spencer, Dane Sullivan <danespen@iu.edu>
2024-04-09 16:15:36 -0400

    more stuff

--- src/main/java/edu/iu/danespen/primesservice/controller/PrimeController.java
+++ src/main/java/edu/iu/danespen/primesservice/controller/PrimeController.java
@@ -1,7 +1,11 @@
 package edu.iu.danespen.primesservice.controller;
 
+import com.nimbusds.jose.proc.SecurityContext;
+
+import org.springframework.security.oauth2.jwt.Jwt;
 import edu.iu.danespen.primesservice.rabbitmq.MQSender;
 import edu.iu.danespen.primesservice.service.IPrimesService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,6 +22,10 @@ public class PrimeController {
     @GetMapping("/{n}")
     public boolean isPrime(@PathVariable int n){
         boolean result = primesService.isPrime(n);
-        mqSender.sendMessage(n, result);
+        Object principal = SecurityContextHolder
+                .getContext().getAuthentication().getPrincipal();
+        String username = ((Jwt) principal).getSubject();
+        System.out.println(username);
+        mqSender.sendMessage(username, n, result);
         return result;
     }}
